5e9e69d92da2821cd97e2afa32531108d118034b
Büşra Keskin <wwbusrakeskin@gmail.com>
2023-03-06 21:36:18 +0300

    Loading other levels and doing delay with using invoke func

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,25 +3,48 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float delayTime = 1f;
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
         {
-            case "Finish":
-                Debug.Log("You've done!");
-                break;
             case "Start":
                 Debug.Log("You've started");
                 break;
-            case "Fuel":
-                Debug.Log("You've bumped with fuel!");
+            case "Finish":
+                StartSuccesSequence();
                 break;
             default:
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                StartCrashSequence();
                 break;
         }
-            
-
+    }
+    void StartSuccesSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delayTime);
+    }
+    void StartCrashSequence()
+    {
+        //TODO: Add SFX upon crash
+        //TODO: Add particle effects
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delayTime);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
     
